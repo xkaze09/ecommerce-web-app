4cb5a678354c4d552f2bd26ddfcee175e9061ce2
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-01-10 04:23:29 +0800

    Add initial Home component for Next.js application

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to Next.js!</h1>
+            <p>This is the default Next.js template.</p>
+        </div>
+    );
+  }
